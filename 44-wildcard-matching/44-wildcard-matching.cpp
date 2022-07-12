40ec6c46b386b3c36f6b137cbacdadfb62f2e59f
Abhishek Jain <55635206+Fiddlly@users.noreply.github.com>
2022-07-12 09:34:26 +0530

    Time: 65 ms (68.86%), Space: 15.1 MB (47.37%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
-        bool isMatch(string s, string p) {
-        vector<vector<char>> mem(s.size()+1,vector<char>(p.size(),-1));
-        return isMatch(0,0,s,p,mem);    
+    bool isMatch(string s, string p) {
+        
+        vector<vector<char>> dp(s.length()+1, vector<char> (p.length(),-1));
+        return isMatch(0,0,s,p,dp);   
     }
-    bool isMatch(int i, int j, string& s, string& p,vector<vector<char>> &mem) {
+    
+    bool isMatch(int i, int j, string& s, string& p, vector<vector<char>> &dp){
+        
         int sn = s.size();
-        if(j==p.size()) return i==sn;
-        if(mem[i][j]!=-1) return mem[i][j];
-        if(p[j]=='*') return mem[i][j]= isMatch(i,j+1,s,p,mem) || (i<sn && isMatch(i+1,j,s,p,mem));
-        if(i<sn && (p[j]=='?'|| s[i]==p[j])) return mem[i][j]=isMatch(i+1,j+1,s,p,mem);
-        return mem[i][j]=0;
+        
+        if(j == p.size()){
+            return i == sn;
+        }
+        
+        if(dp[i][j] != -1)return dp[i][j];
+        
+        
+        if(p[j] == '*'){
+            return dp[i][j] = isMatch(i,j+1, s,p,dp) || (i<sn &&isMatch(i+1,j,s,p,dp));
+        }
+
+        if(i<sn && (p[j] == '?' || p[j] == s[i])){
+            return dp[i][j] = isMatch(i+1,j+1,s,p,dp);
+        }
+                      
+        return dp[i][j] = 0;
     }
 };
